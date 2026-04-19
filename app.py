2ceb864855a67674877f672074926632940655f7
harsinirg <gunasekaranharsini@gmail.com>
2026-04-19 22:14:10 +0530

    Fix port for Render deployment

--- app.py
+++ app.py
@@ -214,7 +214,10 @@ def logout():
     session.clear()
     flash("Logged out successfully.", "success")
     return redirect(url_for("home"))
+ 
 
+import os
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host="0.0.0.0", port=port)
